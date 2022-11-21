198b61e141321bb6d3d0ea585e6459970f46b301
Peng Luo <p2luo@ucsd.edu>
2022-11-20 18:51:42 -0800

    Explore Complete

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Lab 8 - Starter
 
-1) Within a Github action that run wheneer code is pushed. This is clearly the only automated choices among three. 
+Peng Luo
 
+1) Within a Github action that run wheneer code is pushed. This is clearly the only automated choices among three. 
 2) No. It should be unit test level
+3) No. Its functionality heaveily relay on the interaction of individual components of the program.  
+4) Yes. This feature can be checked by testing the message input function alone without interaction without other componenet.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,23 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+test('isPhoneNumber test', () => {
+    expect(functions.isPhoneNumber("8582038254")).toBe(false);
+});
+
+test('isPhoneNumber test', () => {
+    expect(functions.isEmail("p2luo@ucsd.edu")).toBe(true);
+});  
+
+test('isStrongPassword test', () => {
+    expect(functions.isStrongPassword("123456")).toBe(false);
+}); 
+
+test('isDate test', () => {
+    expect(functions.isDate("01012000")).toBe(false);
+}); 
+
+test('isHexColor test', () => {
+    expect(functions.isHexColor("000")).toBe(true);
+}); 
